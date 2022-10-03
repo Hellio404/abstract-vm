15d1db4ce4d2e3a989e8ac41684561741adb3553
Youness FARINI <farini.work@gmail.com>
2022-10-03 17:25:45 +0100

    creating a tokenizer for the abstract-vm basic assembly language.

---
+++ Scanner.cpp
@@ -0,0 +1,176 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Scanner.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yfarini <yfarini@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/03 16:06:34 by yfarini           #+#    #+#             */
+/*   Updated: 2022/10/03 17:25:28 by yfarini          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Scanner.hpp"
+
+const std::unordered_map<std::string, token_type_t>  Scanner::keywords = {
+    {"push",    TOKEN_PUSH},
+    {"pop",     TOKEN_POP},
+    {"dump",    TOKEN_DUMP},
+    {"assert",  TOKEN_ASSERT},
+    {"add",     TOKEN_ADD},
+    {"sub",     TOKEN_SUB},
+    {"mul",     TOKEN_MUL},
+    {"div",     TOKEN_DIV},
+    {"mod",     TOKEN_MOD},
+    {"print",   TOKEN_PRINT},
+    {"exit",    TOKEN_EXIT},
+    {"int8",    TOKEN_INT8},
+    {"int16",   TOKEN_INT16},
+    {"int32",   TOKEN_INT32},
+    {"float",   TOKEN_FLOAT},
+    {"double",  TOKEN_DOUBLE},
+};
+
+Scanner::Scanner(const char *_source): source(_source), current(_source), line(1) 
+{
+    curr_line_start = source;
+    curr_line_end = get_line_end(curr_line_start);
+}
+
+
+Token Scanner::get_next_token()
+{
+    skip_non_tokens();
+    
+    if (reach_end()) return create_token(TOKEN_EOF);
+
+    start = current;
+    char c = advance();
+    switch (c)
+    {
+    case '-': return create_token(TOKEN_MINUS);
+    case '(': return create_token(TOKEN_OPEN_PARENT);
+    case ')': return create_token(TOKEN_CLOSED_PARENT);
+    case ';': advance(); return create_token(TOKEN_EO_STDIN);
+    case '\n':
+        line++;
+        curr_line_start = this->current;
+        curr_line_end = get_line_end(curr_line_start);
+        return create_token(TOKEN_SEP);
+    
+    default:
+        if (isnumber(c)) return number();
+        else if (isalpha(c)) return identifier();
+        return error("use of unknown charachter");
+    }
+}
+
+bool    Scanner::reach_end() const
+{
+    return *current == '\0';
+}
+
+char    Scanner::advance()
+{
+    ++current;
+    return current[-1];
+}
+
+char    Scanner::peek() const
+{
+    return current[0];
+}
+
+char    Scanner::peek_next() const
+{
+    if (current[0])
+        return current[1];
+    return '\0';
+}
+
+Token   Scanner::create_token(token_type_t type) const
+{
+    return (Token) {
+        .type = type,
+        .line = this->line,
+        .start = this->start,
+        .length = static_cast<uint32_t>(this->current - this->start),
+        .error = NULL,
+        .line_start = curr_line_start,
+        .line_end = curr_line_end,
+    };
+}
+
+Token   Scanner::error(const char* message) const
+{
+    return (Token) {
+        .type = TOKEN_ERROR,
+        .line = this->line,
+        .start = this->start,
+        .length = static_cast<uint32_t>(this->current - this->start),
+        .error = message,
+        .line_start = curr_line_start,
+        .line_end = curr_line_end,
+    };
+}
+
+Token   Scanner::number()
+{
+    while (isnumber(peek()))
+        advance();
+    if (peek() == '.')
+    {
+        advance();
+        if (!isnumber(peek()))
+            return error("expected digit after `.`");
+        while (isnumber(peek()))
+            advance();
+        return create_token(TOKEN_REAL_NUMBER);
+    }
+    return create_token(TOKEN_NUMBER);
+}
+
+Token   Scanner::identifier()
+{
+    while (isalnum(peek()))
+        advance();
+    std::string _identifier(this->start, this->current);
+    if (this->keywords.count(_identifier) == 0)
+        return error("use of undeclared keyword");
+    return create_token(this->keywords.at(_identifier));
+}
+
+void    Scanner::skip_non_tokens()
+{
+    while (skip_whitespace() || skip_comments());
+}
+
+bool    Scanner::skip_whitespace()
+{
+    char c = peek();
+    if (c != ' ' && c != '\t')
+        return false;
+    while ((c = peek()) && (c == ' ' || c == '\t'))
+        advance();
+    return true;
+}
+
+bool    Scanner::skip_comments()
+{
+    if (peek() == ';' && peek_next() != ';')
+    {
+        char c;
+        while ((c = peek()) && c != '\n')
+            advance();
+        return true;
+    }
+    return false;
+}
+
+
+const char  *Scanner::get_line_end(const char *curr) const
+{
+    while (*curr && *curr != '\n')
+        ++curr;
+    return curr;
+}
---
+++ Scanner.hpp
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Scanner.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yfarini <yfarini@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/03 14:39:06 by yfarini           #+#    #+#             */
+/*   Updated: 2022/10/03 17:19:12 by yfarini          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+# include <cinttypes>
+# include <string>
+# include <unordered_map>
+
+typedef enum token_type {
+    TOKEN_PUSH,
+    TOKEN_POP,
+    TOKEN_DUMP,
+    TOKEN_ASSERT,
+    TOKEN_PRINT,
+    TOKEN_EXIT,
+
+    TOKEN_ADD,
+    TOKEN_SUB,
+    TOKEN_MUL,
+    TOKEN_DIV,
+    TOKEN_MOD,
+
+    TOKEN_INT8,
+    TOKEN_INT16,
+    TOKEN_INT32,
+    TOKEN_INT64,
+    TOKEN_FLOAT,
+    TOKEN_DOUBLE,
+
+    TOKEN_OPEN_PARENT,
+    TOKEN_CLOSED_PARENT,
+
+    TOKEN_MINUS,
+    TOKEN_NUMBER,
+    TOKEN_REAL_NUMBER,
+
+    TOKEN_ERROR,
+    TOKEN_SEP,
+    TOKEN_EOF,
+    TOKEN_EO_STDIN,
+}           token_type_t;
+
+struct Token {
+    token_type_t    type;
+    uint32_t        line;
+    const char      *line_start;
+    const char      *line_end;
+    const char      *start;
+    const char      *error;
+    uint32_t        length;
+
+    Token() = default;
+    ~Token() = default;
+    Token(const Token&) = default;
+    Token& operator=(const Token&) = default;
+};
+
+class Scanner
+{
+private:
+    const char          *source;
+    uint32_t            line;
+    // the start of the line being parsed
+    const char          *curr_line_start;
+    const char          *curr_line_end;
+    // the start of the current token being parsed
+    const char          *start;
+    // the next characted needed to be parsed
+    const char          *current;
+
+    // map each known keyword to a token_type
+    static const std::unordered_map<std::string, token_type_t>  keywords;
+public:
+    Scanner() = delete;
+    ~Scanner() = default;
+    Scanner(const Scanner&) = default;
+    Scanner& operator =(const Scanner&) = default;
+
+    Scanner(const char *);
+    Token   get_next_token();
+private:
+
+    bool    reach_end() const;
+    // return the next character and increment `this->current`
+    char    advance();
+    // return the next character without incrementing `this->current`
+    char    peek() const;
+    // return the  character after the next one without incrementing `this->current`
+    char    peek_next() const;
+
+    // given a type of a token, create a token with start = `this->start` and length of `this->current - this->start`
+    Token   create_token(token_type_t) const;
+    // given an error message create a token of type TOKEN_ERROR and set it's error field to the given error message
+    Token   error(const char* message) const;
+    // read a number (int or float) and return a token of type TOKEN_NUMBER or TOKEN_REAL_NUMBER depending on if the number is a floating point
+    Token   number();
+    // read an identifier, if it's an known keyword return it's type as the token type if not return a token of type error
+    Token   identifier();
+
+    void    skip_non_tokens();
+    bool    skip_whitespace();
+    bool    skip_comments();
+
+    const char  *get_line_end(const char *) const;
+};
